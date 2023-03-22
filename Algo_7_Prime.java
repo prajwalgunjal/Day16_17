93b0617c12bef5b51e5f876feaa2159d3878a8db
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-22 22:15:34 +0530

    Prime number 0-1000

---
+++ Algo_7_Prime.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+public class Algo_7_Prime {
+    static int num=0;
+    void prime() {
+        System.out.println("Printing prime numbers between 0-1000 ");
+        int count=0;
+        for (int i = 2; i < 1000; i++) {
+             num = i;
+             print();
+        }
+    }
+    void print(){
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            System.out.print(num + " ");
+
+        }
+    }
+
+    public static void main(String[] args) {
+    Algo_7_Prime algo7Prime = new Algo_7_Prime();
+    algo7Prime.prime();
+    }
+}
