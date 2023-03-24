3bdeb12adfb765b6cbe0cbffbef419062201829f
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-24 20:21:05 +0530

    2nd program

---
+++ DSAProgram_2_ordered/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.DSAProgram_2_ordered;
+
+public class  Node <T> {
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public Node() {
+    }
+}
