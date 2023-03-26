0221768139a25a69072ea86a905110d28a9602dc
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-26 15:10:31 +0530

    updated

--- Algo_10_FindNumber.java
+++ Algo_10_FindNumber.java
@@ -4,29 +4,27 @@ import java.util.Scanner;
 
 public class Algo_10_FindNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number :-");
-        int h = sc.nextInt();
-        int low =0;
-        int high= (int)Math.pow(2,h);
-        System.out.printf("Think of a number between 0 and %d: ", high - 1);
-        int secretNumber = sc.nextInt();
-//        System.out.println(high);
-        boolean flag = false;
-        while(!flag){
-            int guess = ((low +high)/2);
-            System.out.println("is your number is less than or equal to "+guess+"(Y/N)");
-            String user_response = sc.next();
-            if(user_response=="y"){
-                high=(guess+1);
-            }
-            else {
-                low=(guess-1);
-            }
-            if(low==high)
-            {
-                System.out.println("your magic number is "+low);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENter a number");
+        int n = scanner.nextInt();
+        int low = 0, high = n - 1;
+        int guess = 0, numGuesses = 0;
+
+        System.out.print("Think of a number between 0 and " + (n - 1) + ".\n");
+        while (low <= high) {
+            guess = (low + high) / 2;
+            System.out.print("Is your number between " + low + " and " + guess + "? ");
+            String str = scanner.next();
+            numGuesses++;
+
+            if (str.equals("y")) {
+                high = guess - 1;
+            } else {
+                    low = guess + 1;
             }
         }
+        System.out.println("Your number is " + guess + ".");
+        System.out.println("Number of guesses: " + numGuesses);
     }
 }
