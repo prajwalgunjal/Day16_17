82414a849a5e31a89467f7bd92be83ce763f0881
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-26 18:02:27 +0530

    Extend calender

---
+++ DSAProgram_13_ExtendCalender/WeekDay.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.DSAProgram_13_ExtendCalender;
+
+public class WeekDay {
+    String day;
+    String date;
+
+    public WeekDay(String day, String date) {
+        this.day = day;
+        this.date = date;
+    }
+
+    public String toString() {
+        return day + " " + date;
+    }
+}
